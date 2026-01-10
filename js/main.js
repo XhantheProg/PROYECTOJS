45066e1b17b24e026b1b2c659922fd1a6c2c89f8
XhantheProg <Curlydoge107@gmail.com>
2026-01-10 12:04:20 -0400

    Refactor navigation event listeners to use menu items for routing

--- js/main.js
+++ js/main.js
@@ -6,11 +6,18 @@ const router =new Router(app);
 
 router.navigate("dashboard"); //navegar a la pagina de dashboard al iniciar
 
-document.getElementById("dash-btn").addEventListener("click", ()=>{
-    router.navigate("dashboard")
-});
-document.getElementById("prod-btn").addEventListener("click", ()=>{
-    router.navigate("products")
+// document.getElementById("dash-btn").addEventListener("click", ()=>{
+//     router.navigate("dashboard")
+// });
+// document.getElementById("prod-btn").addEventListener("click", ()=>{
+//     router.navigate("products")
     
-});
+// });
 
+
+document.querySelectorAll(".menu-item").forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        const page=button.dataset.page; //obtener el nombre de la pagina desde el atributo data-page
+        router.navigate(page);
+    });
+});
