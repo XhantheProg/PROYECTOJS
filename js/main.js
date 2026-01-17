d1063b8904c35f9c6b2615c0f888c9f893e5970a
XhantheProg <Curlydoge107@gmail.com>
2026-01-17 14:17:20 -0400

    Fix import path for products service and clean up commented code in main.js

--- js/main.js
+++ js/main.js
@@ -1,63 +1,22 @@
 import { Router } from "./router.js";
-import { initProducts } from "./services/poducts_service.js";
+import { initProducts } from "./services/products.service.js";
 
-const app = document.getElementById('app');
-const router = new Router(app);
+const app= document.getElementById("app")
+const router = new Router(app)
 initProducts()
+//todo  funcion para marcar activo en el sidebar
+function setActive(page) { //dashboard
+    document.querySelectorAll(".menu-item").forEach((btn)=>{
+        btn.classList.toggle("active", btn.dataset.page===page)
+    })
+} 
 
-//funcion para marcar activo el dashboard
-
-function setActive(page){ //dashboard
-    document.querySelectorAll('.menu-item').forEach((btn)=>{
-        btn.classList.toggle('active', btn.dataset.page === page); //classlist es para manejar las clases de css
-        // toggle agrega o quita una clase dependiendo si ya la tiene o no
-        //btn.dataset.page accede al atributo data-page del boton
-    });
-
-    
-}
-
-{/* <button> id="dash-btn" class="menu-item" data-page="dashboard" style="border: 1px solid red">Dashboard</button> */}
-
-
-
-
-
-
-
-
-
-
-
-// router.navigate("dashboard");
-
-// document.getElementById('dash-btn').addEventListener("click",(e)=>{
-//     e.preventDefault();
-//     router.navigate("dashboard");
-// });
-// document.getElementById('prod-btn').addEventListener("click",(e)=>{
-//     e.preventDefault();
-//     router.navigate("products");
-// });
-
-
-
-// todo funcion para navegar cuando hacemos click en el sifebar
-document.querySelectorAll('.menu-item').forEach((btn)=>{
-    btn.addEventListener("click",()=>{
-        const page = btn.dataset.page; //acceder al atributo data-page
-        router.navigate(page); //navegar a la pagina correspondiente
-        setActive(page); //marcar el boton como activo
-    });
-});
-        
-
-router.navigate("dashboard");
-
-
-// document.querySelectorAll('.menu-item').forEach((btn)=>{
-//     btn.addEventListener("click",(e)=>{
-//         const page = btn.dataset.page;
-//         router.navigate(page);
-//     })
-// });
+//todo  funcion para navegar cuando hacemos click en el sidebar
+document.querySelectorAll(".menu-item").forEach(btn=>{
+        btn.addEventListener("click",()=>{
+            const page = btn.dataset.page //dashboard
+            router.navigate(page)
+            setActive(page)
+        })
+})
+router.navigate("dashboard")
